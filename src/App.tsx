e4d9c16c463f1a1afea136d5022a365c03ebb1f5
Progress Aienobe <paienobe@gmail.com>
2023-10-09 22:05:09 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { MysticSdk } from "mystic-sdk-1";
+
+function App() {
+  return (
+    <>
+      <MysticSdk
+        token={
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGQyMWQzMGUyMjkxNDdiNjY1ZGM3YSIsImlhdCI6MTY5MjIxMzcxNX0.2RZ8He93TUUMeW0t3aqpt5KxNlo1r1fjMNtaBYyP9rI"
+        }
+      />
+    </>
+  );
+}
+
+export default App;
